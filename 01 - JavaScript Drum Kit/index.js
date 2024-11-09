b7d8b5bdc32113f99080840fdbe87885feeb7e54
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2024-11-09 17:02:26 +0530

    UPDATE 01-JavaScriptDrumKit

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,12 +1,3 @@
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; // stop the function from running all together.
-    audio.currentTime = 0; // rewind to the start.
-    audio.play();
-    key.classList.add('playing');
-});
-
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return; // skip it if it's no a transform.
     this.classList.remove('playing');
@@ -14,3 +5,13 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", function (e) {  
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running all together.
+  
+  key.classList.add("playing");
+  audio.currentTime = 0; // rewind to the start.
+  audio.play();
+});
